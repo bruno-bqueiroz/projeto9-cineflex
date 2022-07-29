29f26b570aa1e9a26c61105444a070b69cc12aba
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-07-29 12:54:52 -0400

    input adicionado

--- src/Compenentes/Assentos.js
+++ src/Compenentes/Assentos.js
@@ -2,6 +2,35 @@ import {useState, useEffect} from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
+
+function Reservar(){
+    const [nome, setNome] = useState("");
+	const [cpf, setCPF] = useState("");
+   
+    function fazerReserva(event){
+        event.preventDefault();
+        const requisicao = axios.post("https://mock-api.driven.com.br/api/v7/cineflex/seats/book-many",{
+            ids: [1, 2, 3],
+            name: nome,
+            cpf: cpf
+        });
+    }
+
+    return(
+        <>
+        <form onSubmit={fazerReserva}>
+            <p>Nome do comprador:</p>
+		        <input type="nome" value={nome} onChange={e => setNome(e.target.value)} />
+            <p>CPF do comprador:</p>
+               
+                <input type="cpf" value={cpf} onChange={e => setCPF(e.target.value)} />
+               
+            <button type="submit">Reservar assento(s)</button>
+		</form>
+        </>
+    )
+}
+
 export default function Assentos(){
      const [rodape, setRodape] = useState([]);
      const [horaRodape, setHoraRodape] = useState([]);
@@ -43,7 +72,10 @@ export default function Assentos(){
                     <div className='caixas'>
                         <div className="bolinha indisponivel"></div>
                           <p>indisponivel</p>
+                        </div>
                     </div>
+                    <div className='input'>
+                    <Reservar/>
                     </div>
             </div>
             <div className="escolhido">
